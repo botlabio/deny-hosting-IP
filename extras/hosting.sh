9015aa2391d8f8ac50c51b0a7019de2a5db1cfc7
Mikko Kotila <mailme@mikkokotila.com>
2017-05-07 19:37:09 +0300

    Update hosting.sh

--- extras/hosting.sh
+++ extras/hosting.sh
@@ -1,4 +1,4 @@
 while read IP
 	do
-	mysql -u root -ppu8x3+zmHAU+nNw9f0FeKM1dgfg= -h localhost hosting_ip -e "SELECT * FROM blacklist WHERE ip='$IP'";
+	mysql -u root -pPASSWORD -h localhost hosting_ip -e "SELECT * FROM blacklist WHERE ip='$IP'";
 done <hosting.input
